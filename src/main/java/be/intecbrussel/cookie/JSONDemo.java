c1e61b2e5b867cb1870bb20a12489f71ec6eeb74
Mert <mertdemirok80@gmail.com>
2021-04-23 18:24:03 +0200

    Convert Cookie String to JSONObject and vice versa

---
+++ src/main/java/be/intecbrussel/cookie/JSONDemo.java
@@ -0,0 +1,17 @@
+package be.intecbrussel.cookie;
+
+import org.json.Cookie;
+import org.json.JSONObject;
+
+public class JSONDemo {
+    public static void main(String[] args) {
+        String cookie = "username = Mark Den; expires = Thu, 15 Jun 2020 12:00:00 UTC; path = /";
+
+        //Case 1: Converts Cookie String to JSONObject
+        JSONObject jsonObject = Cookie.toJSONObject(cookie);
+        System.out.println(jsonObject);
+
+        //Case 2: Converts JSONObject to Cookie String
+        System.out.println(Cookie.toString(jsonObject));
+    }
+}
