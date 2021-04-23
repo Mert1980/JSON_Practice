c6dd288b6a3ec4e85a8f2e6370c99013ce91167d
Mert <mertdemirok80@gmail.com>
2021-04-23 17:43:36 +0200

    add example

---
+++ src/main/java/be/intecbrussel/cdl/JSONDemo.java
@@ -0,0 +1,33 @@
+package be.intecbrussel.cdl;
+
+import org.json.JSONArray;
+import org.json.CDL;
+import org.json.JSONTokener;
+
+public class JSONDemo {
+    public static void main(String[] args) {
+        String csvData = "INDIA, UK, USA";
+
+        //Case 1: CSV to JSON Array
+        JSONArray jsonArray = CDL.rowToJSONArray(new JSONTokener(csvData));
+        System.out.println(jsonArray);
+
+        //Case 2: JSONArray to CSV
+        System.out.println(CDL.rowToString(jsonArray));
+
+        //Case 3: CSV to JSONArray of Objects
+        csvData = "empId, name, age \n" +
+                "1, Mark, 22 \n" +
+                "2, Robert, 35 \n" +
+                "3, Julia, 18";
+        System.out.println(CDL.toJSONArray(csvData));
+
+        //Case 4: CSV without header
+        jsonArray = new JSONArray();
+        jsonArray.put("name");
+        jsonArray.put("age");
+        jsonArray.put("empId");
+        csvData = "1, Mark, 22 \n" + "2, Robert, 35 \n" + "3, Julia, 18";
+        System.out.println(CDL.toJSONArray(jsonArray,csvData));
+    }
+}
