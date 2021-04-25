dd53773d077c34094785c56d8c0b40f3e8f6204b
Mert <mertdemirok80@gmail.com>
2021-04-25 20:32:45 +0200

    ignoring the property id from serialization

---
+++ src/test/java/be/intecbrussel/jackson/e14_json_ignore_properties/TestJsonIgnoreProperties.java
@@ -0,0 +1,25 @@
+package be.intecbrussel.jackson.e14_json_ignore_properties;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestJsonIgnoreProperties {
+    @Test
+    public void whenSerializingUsingJsonIgnoreProperties_thenCorrect()
+            throws JsonProcessingException {
+
+        BeanWithIgnore bean = new BeanWithIgnore(1, "My bean");
+
+        String result = new ObjectMapper()
+                .writeValueAsString(bean);
+
+        System.out.println(result); // {"name":"My bean"}
+        assertThat(result, containsString("My bean"));
+        assertThat(result, not(containsString("id")));
+    }
+}
