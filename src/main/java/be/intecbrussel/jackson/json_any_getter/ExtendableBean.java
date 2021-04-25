9271507a42b296a2dcfd4cf7137929d67f7306b6
Mert <mertdemirok80@gmail.com>
2021-04-25 09:28:41 +0200

    test @JsonAnyGetter annotation

---
+++ src/main/java/be/intecbrussel/jackson/json_any_getter/ExtendableBean.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.jackson.json_any_getter;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExtendableBean {
+    public String name;
+    private Map<String, String> properties;
+
+    public ExtendableBean(String name) {
+        this.properties = new HashMap<>();
+        this.name = name;
+    }
+
+    @JsonAnyGetter
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public void add(String attr1, String val1) {
+        properties.put(attr1, val1);
+    }
+}
---
+++ src/test/java/be/intecbrussel/jackson/json_any_better/Test.java
@@ -0,0 +1,25 @@
+package be.intecbrussel.jackson.json_any_better;
+import be.intecbrussel.jackson.json_any_getter.ExtendableBean;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Test {
+
+    @org.junit.Test
+    public void whenSerializingUsingJsonAnyGetter_thenCorrect()
+            throws JsonProcessingException {
+
+        ExtendableBean bean = new ExtendableBean("My bean");
+        bean.add("attr1", "val1");
+        bean.add("attr2", "val2");
+
+        String result = new ObjectMapper().writeValueAsString(bean);
+        System.out.println(bean.getProperties());
+        System.out.println(result);
+
+        assertThat(result, containsString("attr1"));
+        assertThat(result, containsString("val1"));
+    }
+}
